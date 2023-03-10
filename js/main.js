19c3271fff70e92758101ad5f0a5e99932f85065
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-03-10 16:35:34 +0600

    pause and reset btn working

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-let [seconds, minutes, hours] = [0,0,0];
+let [milliseconds,seconds, minutes, hours] = [0,0,0,0];
 const displayTime = document.getElementById('display-time');
+const watchStartBtn = document.getElementById('watch-start');
 let timer = null;
 const stopWatch = () =>{
-    seconds++;
-    if(seconds === 60){
-        seconds = 0;
-        minutes++;
-        if(minutes === 60){
-            minutes = 0;
-            hours++;
+    /* milliseconds += 10;
+    if(milliseconds === 1000){
+        milliseconds = 0; */
+        seconds++;
+        if(seconds === 60){
+            seconds = 0;
+            minutes++;
+            if(minutes === 60){
+                minutes = 0;
+                hours++;
+            }
         }
-    }
-    displayTime.innerHTML = `${hours < 10 ? '0'+hours : hours}:${minutes < 10 ? '0'+minutes : minutes}:${seconds < 10 ? '0'+seconds : seconds}` 
+    // }
+    const milliSeconds = milliseconds % 10;
+    displayTime.innerHTML = `${hours < 10 ? '0'+hours : hours}:${minutes < 10 ? '0'+minutes : minutes}:${seconds < 10 ? '0'+seconds : seconds}:${milliseconds < 100 ? milliseconds : milliSeconds}` 
 }
 const watchStart = () =>{
     if(timer !== null){
@@ -19,3 +25,16 @@ const watchStart = () =>{
     }
     timer = setInterval(stopWatch, 1000);
 }
+const watchPause = () =>{
+    clearInterval(timer);
+    watchStartBtn.innerText = "Go on"
+}
+const watchReset = () =>{
+    clearInterval(timer);
+    [milliseconds,seconds, minutes, hours] = [0,0,0,0];
+    displayTime.innerHTML = "00:00:00";
+}
+watchStartBtn.addEventListener('click', () =>{
+    watchStartBtn.innerText = "Start";
+    watchStart();
+})
